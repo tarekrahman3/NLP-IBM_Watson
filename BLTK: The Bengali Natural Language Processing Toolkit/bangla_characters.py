b66f68f8bac191b29ae54b6dcb822ac30ad213ff
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-11-22 23:54:14 +0600

    Create bangla_characters.py

---
+++ BLTK: The Bengali Natural Language Processing Toolkit/bangla_characters.py
@@ -0,0 +1,14 @@
+from bltk.langtools.banglachars import (vowels,
+                                        vowel_signs,
+                                        consonants,
+                                        digits,
+                                        operators,
+                                        punctuations,
+                                        others)
+print(f'Vowels: {vowels}')
+print(f'Vowel signs: {vowel_signs}')
+print(f'Consonants: {consonants}')
+print(f'Digits: {digits}')
+print(f'Operators: {operators}')
+print(f'Punctuation marks: {punctuations}')
+print(f'Others: {others}')
